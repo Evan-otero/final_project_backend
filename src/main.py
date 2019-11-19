3dcf6fa9743e019febb918442f80317d70b55f15
Evan Otero <Evan.otero19@gmail.com>
2019-11-19 01:38:31 +0000

    initial commit

--- src/main.py
+++ src/main.py
@@ -7,7 +7,12 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db
+from models import db, Users, Locations
+from flask_jwt_simple import (
+    JWTManager, jwt_required, create_jwt, get_jwt_identity
+)
+from sqlalchemy.exc import SQLAlchemyError
+
 #from models import Person
 
 app = Flask(__name__)
@@ -17,6 +22,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
+
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
+jwt = JWTManager(app)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -37,7 +45,54 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@app.route('/users', methods=['GET'])
+def handle_users():
+    all_people = Users.query.all()
+    all_people = list(map(lambda x: x.serialize(), all_people))
+    return jsonify(all_people), 200
+
+@app.route('/adduser', methods=['POST'])
+def handle_user():
+    try:
+        body = request.get_json()
+        user1 = Users(username=body['username'], email=body['email'], password=body['password'],name=body['name'],)
+        db.session.add(user1)
+        db.session.commit()
+        
+    except SQLAlchemyError as e:
+        return jsonify({ "error": str(e.__dict__['orig'])}), 409
+    return jsonify({"message":"success"})
+
+@app.route('/login', methods=['POST'])
+def login():
+    try:
+        if not request.is_json:
+            return jsonify({"msg":"Missing JSON in request"}), 400
+
+        params = request.get_json()
+        username = params.get('username', None)
+        password = params.get('password', None)
+
+        if not username:
+            return jsonify({"msg": "Missing username parameter"}), 400
+        if not password:
+            return jsonify({"msg": "Missing password parameter"}), 400
+        
+        usercheck = Users.query.filter_by(username=username, password=password).first()
+        if usercheck == None:
+            return jsonify({"msg": "Bad username or password"}), 401
+        ret = {'jwt': create_jwt(identity=username), "id": usercheck.id, "email": usercheck.email}
+        return jsonify(ret), 200
+    except SQLAlchemyError as e:
+        return jsonify({ "error": str(e.__dict__['orig'])}), 409
+    
+    return jsonify({"message": "success"}),200
+@app.errorhandler(APIException)
+def handle_invalid_usage(error):
+    return jsonify(error.to_dict()), error.status_code
+
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
--- src/models.py
+++ src/models.py
@@ -2,16 +2,29 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class Users(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     username = db.Column(db.String(80), unique=True, nullable=False)
+     email = db.Column(db.String(120), unique=True, nullable=False)
+     name = db.Column(db.String(20),unique= False, nullable=True)
+     password= db.Column(db.String(20),unique=False, nullable=False)
+     image= db.Column(db.String(250), unique=False, nullable=True)
+     
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+     def serialize(self):
+         return {
+             "username": self.username,
+             "email": self.email,
+             "id" : self.id,
+             "name": self.name,
+             "password": self.password,
+         }
+class Locations(db.Model):
+        id= db.Column(db.Integer, primary_key=True)
+        title = db.Column(db.String(80), unique=False, nullable=False)
+        address = db.Column(db.String(80), unique=True, nullable=False)
+        lat = db.Column(db.String(20),unique= False, nullable=False)
+        log= db.Column(db.String(20),unique= False, nullable=False)
+        ratings= db.Column(db.String(20),unique=False, nullable=False)
+        fenced= db.Column(db.String(250), unique=False, nullable=False)
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
